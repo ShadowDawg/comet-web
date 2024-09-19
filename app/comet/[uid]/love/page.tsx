7d296f3c180b99f51a4ef8a17124932011e145da
dev <devmandal2004@gmail.com>
2024-09-19 16:52:49 +0530

    added hardcode for unmatched

--- app/comet/[uid]/love/page.tsx
+++ app/comet/[uid]/love/page.tsx
@@ -10,11 +10,14 @@ export default async function LovePage({ params }: { params: { uid: string } }) 
   if (!matchUid) {
     return (
       <div className="text-center p-8">
-      <h2 className="text-2xl font-bold mb-4 font-playwrite text-yellow-400">Love is in the air.</h2>
-      <p className="text-lg mb-6">Every week, we consult the cosmos (and our algorithms) to find your celestial soulmate in insti. Or at least someone who won&apos;t ghost you immediately.</p>
+      <h2 className="text-2xl font-bold mb-4 font-playwrite text-yellow-400">Love will be in the air next Week.</h2>
+      <p className="text-lg mb-6">The stars indicate that you'll enjoy your own company this week. Defy the stars at the speed-dating booth at the AfterParty this Saturday.</p>
       <Card className="inline-block bg-yellow-300 shadow-md">
         <CardContent className="p-3 flex items-center justify-center h-20 w-48">
-          <p className="text-sm font-semibold text-black">Every Thursday 4:20PM</p>
+          <p className="text-sm font-semibold text-black">The AfterParty</p>
+          <p className="text-sm text-gray-600 mt-1">Saturday, 21st September</p>
+          <p className="text-sm text-gray-600 mt-1">CLT, 8 to 10PM</p>
+
         </CardContent>
       </Card>
     </div>
